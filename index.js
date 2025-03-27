a714fb96cddff332ac54bb4a1ed26496963dfd7c
dat.dgw-dev <datthanh.dgw@gmail.com>
2025-03-27 09:31:49 +0700

    * up

--- index.js
+++ index.js
@@ -2,8 +2,30 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({ extended: true })); // Middleware để parse form data
+
 app.get('/', (req, res) => {
-  res.send('Hello World! Chào 2 bạn!');
+  res.send(`
+    <form action="/login" method="POST">
+      <label for="username">Username:</label>
+      <input type="text" id="username" name="username" required>
+      <br>
+      <label for="password">Password:</label>
+      <input type="password" id="password" name="password" required>
+      <br>
+      <button type="submit">Login</button>
+    </form>
+  `);
+});
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  // Xử lý đăng nhập ở đây
+  if (username === 'admin' && password === 'password') {
+    res.send('Login successful!');
+  } else {
+    res.send('Invalid username or password.');
+  }
 });
 
 app.listen(port, () => {
